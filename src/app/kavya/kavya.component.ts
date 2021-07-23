0f30a692b714f30c66ce48942f3ab42243d04427
Rishi Sharma <rishisharma@Rishis-MacBook-Air.local>
2021-07-23 20:59:15 +0530

    update

---
+++ src/app/kavya/kavya.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kavya',
+  templateUrl: './kavya.component.html',
+  styleUrls: ['./kavya.component.css']
+})
+export class KavyaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
